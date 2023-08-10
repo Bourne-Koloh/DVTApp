21031810f9c1a0874ba56678bc59b46afdd63751
Bourne Koloh <singi.bornface@eclectics.io>
2023-08-10 15:53:52 +0300

    - Added untracked files to git

---
+++ weatherappUITests/weatherappUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  weatherappUITestsLaunchTests.swift
+//  weatherappUITests
+//
+//  Created by Bourne Koloh on 06/08/2023.
+//  Email : bournekolo@icloud.com
+//
+
+import XCTest
+
+final class weatherappUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
